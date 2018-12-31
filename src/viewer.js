084ccb05f29765dc866f8368bbae5dbe28ae226a
Nick Coutsos <nick@coutsos.com>
2018-12-31 17:45:49 -0500

    Add shadows

--- src/viewer.js
+++ src/viewer.js
@@ -3,9 +3,13 @@ import {
   AmbientLight,
   DirectionalLight,
   PerspectiveCamera,
+  Mesh,
+  PlaneGeometry,
+  ShadowMaterial,
   Scene,
   Vector3,
-  WebGLRenderer
+  WebGLRenderer,
+  PCFSoftShadowMap
 } from 'three'
 
 const container = document.querySelector('#app')
@@ -14,11 +18,18 @@ export const renderer = new WebGLRenderer({ antialias: true, alpha: true });
 export const camera = new PerspectiveCamera(75, 1, 0.1, 1000)
 export const scene = new Scene()
 
+const plane = new Mesh(
+  new PlaneGeometry(10, 10),
+  new ShadowMaterial({ opacity: 0.5 })
+)
+
 export const renderFrame = throttle(() => {
   renderer.render(scene, camera)
 }, 16)
 
 export const init = () => {
+  renderer.shadowMap.enabled = true
+  renderer.shadowMap.type = PCFSoftShadowMap
   renderer.setPixelRatio(window.devicePixelRatio ? window.devicePixelRatio : 1)
   container.appendChild(renderer.domElement)
   window.addEventListener('resize', resize)
@@ -26,7 +37,14 @@ export const init = () => {
   const ambientLight = new AmbientLight('powderblue', .8)
   const directionalLight = new DirectionalLight('white', 1)
   ambientLight.name = 'ambient'
-  directionalLight.position.set(0, 5, 5)
+  directionalLight.position.set(5, 5, 5)
+  directionalLight.castShadow = true
+  directionalLight.lookAt(0, 0, 0)
+  directionalLight.shadow.mapSize.width = 1024
+  directionalLight.shadow.mapSize.height = 1024
+
+  plane.rotation.x = -Math.PI/2
+  plane.receiveShadow = true
 
   camera.position.set(-5, -10, 10)
   camera.lookAt(new Vector3(0, 4, 0))
@@ -34,7 +52,8 @@ export const init = () => {
   scene.add(
     camera,
     directionalLight,
-    ambientLight
+    ambientLight,
+    plane
   )
 
   resize()
