290be778a1245ad1fce0cfffab037cc319bbece4
Nick Coutsos <nick@coutsos.com>
2019-01-13 22:35:35 -0500

    Refactor slideshow module

--- src/slideshow.js
+++ src/slideshow.js
@@ -2,16 +2,18 @@ import EventEmitter from 'eventemitter3'
 import { upperFirst } from 'lodash'
 
 export const events = new EventEmitter()
+const slides = []
+const state = {
+  slide: 0,
+  previousSlide: -1,
+  fragment: -1,
+  previousFragment: -1
+}
 
 export const initialize = () => {
-  const slides = [].slice.call(document.querySelectorAll('section.slide'))
-  const state = {
-    slide: 0,
-    fragment: -1
-  }
-
+  slides.push(...[].slice.call(document.querySelectorAll('section.slide')))
   slides[state.slide].classList.add('active')
-  updateAppState(slides, state)
+  updateAppState()
 
   events.emit('slidechanged', {
     previousSlideIndex: state.previousSlide,
@@ -21,105 +23,107 @@ export const initialize = () => {
   })
 
   window.addEventListener('keyup', ({ key }) => {
-    if (key === 'ArrowRight' || key === ' ') next(slides, state)
-    else if (key === 'ArrowLeft') prev(slides, state)
+    if (key === 'ArrowRight' || key === ' ') next()
+    else if (key === 'ArrowLeft') prev()
+  })
+
+  events.on('swipe', ({ direction }) => {
+    if (direction === 'right') {
+      prev()
+    } else if (direction === 'left') {
+      next()
+    }
   })
 }
 
-export const next = (slides, state) => {
-  const fragments = [].slice.call(slides[state.slide].querySelectorAll('.fragment'))
+export const next = () => {
+  const fragments = slides[state.slide].querySelectorAll('.fragment')
 
   if (state.fragment < fragments.length - 1) {
     state.previousFragment = state.fragment
     state.fragment++
-    const previousFragment = fragments[state.previousFragment]
-    const fragment = fragments[state.fragment]
-    const formerCurrent = document.querySelector('.fragment.current')
-    fragment.classList.add('active', 'current')
-
-    if (formerCurrent) {
-      formerCurrent.classList.remove('current')
-    }
-
-    events.emit('fragmentchanged', {
-      slide: slides[state.slide],
-      slideIndex: state.slide,
-      previousFragmentIndex: state.fragment - 1,
-      previousFragment,
-      fragment,
-      state
-    })
+    emitFragmentChange()
   } else if (state.slide < slides.length - 1) {
     state.previousSlide = state.slide
     state.slide++
-
-    const previousSlide = slides[state.previousSlide]
-    const slide = slides[state.slide]
-
-    previousSlide.classList.remove('active')
-    slide.classList.add('active')
     state.fragment = -1
     state.previousFragment = -1
-
-    events.emit('slidechanged', {
-      previousSlideIndex: state.previousSlide,
-      previousSlide,
-      slide,
-      state
-    })
+    emitSlideChange()
   }
 
-  updateAppState(slides, state)
+  updateAppState()
 }
 
-export const prev = (slides, state) => {
+export const prev = () => {
   if (state.fragment > -1) {
     state.previousFragment = state.fragment
     state.fragment--
-    const fragments = [].slice.call(slides[state.slide].querySelectorAll('.fragment'))
-    const previousFragment = fragments[state.previousFragment]
-    const fragment = fragments[state.fragment]
 
-    previousFragment.classList.remove('active')
-    events.emit('fragmentchanged', {
-      slide: slides[state.slide],
-      previousFragmentIndex: state.fragment + 1,
-      previousFragment,
-      fragment,
-      state
-    })
+    emitFragmentChange()
   } else if (state.slide > 0) {
     state.previousSlide = state.slide
     state.slide--
 
-    const previousSlide = slides[state.previousSlide]
-    const slide = slides[state.slide]
-    const fragments = [].slice.call(slide.querySelectorAll('.fragment'))
+    state.previousFragment = state.fragment = slides[state.slide]
+      .querySelectorAll('.fragment')
+      .length - 1
 
-    previousSlide.classList.remove('active')
-    slide.classList.add('active')
-    state.fragment = fragments.length - 1
-    state.previousFragment = state.fragment
-
-    events.emit('slidechanged', {
-      previousSlideIndex: state.slide + 1,
-      previousSlide,
-      slide,
-      state
-    })
-
-    updateAppState(slides, state)
+    emitSlideChange()
   }
+
+  updateAppState()
 }
 
-const updateAppState = (slides, state) => {
+const emitSlideChange = () => {
+  events.emit('slidechanged', {
+    previousSlideIndex: state.previousSlide,
+    previousSlide: slides[state.previousSlide],
+    slide: slides[state.slide],
+    state
+  })
+}
+
+const emitFragmentChange = () => {
+  const fragments = Array.from(
+    slides[state.slide].querySelectorAll('.fragment')
+  )
+
+  events.emit('fragmentchanged', {
+    slide: slides[state.slide],
+    previousFragmentIndex: state.previousFragment,
+    previousFragment: fragments[state.previousFragment],
+    fragment: fragments[state.fragment],
+    state
+  })
+}
+
+const updateAppState = () => {
   const app = document.querySelector('#app')
   const slide = slides[state.slide]
-  const fragment = state.fragment > 0 && (
-    slide.querySelector(`.fragment:nth-child(${state.fragment - 1})`)
-  )
+  const fragments = Array.from(slide.querySelectorAll('.fragment'))
+  const fragment = state.fragment > 0 && fragments[state.fragment + 1]
+
+  slide.classList.add('active')
+  if (state.previousSlide !== -1){
+    slides[state.previousSlide].classList.remove('active')
+  }
+
+  for (let i = 0; i < fragments.length; i++) {
+    if (i === state.fragment) {
+      fragments[i].classList.add('active', 'current')
+    } else if (i < state.fragment) {
+      fragments[i].classList.add('active')
+      fragments[i].classList.remove('current')
+    } else {
+      fragments[i].classList.remove('active', 'current')
+    }
+  }
 
   for (let prop in app.dataset) { delete app.dataset[prop] }
   for (let prop in slide.dataset) { app.dataset[`slideState${upperFirst(prop)}`] = slide.dataset[prop] }
-  if (fragment) for (let prop in fragment.dataset) { app.dataset[`fragmentState${upperFirst(prop)}`] = fragment.dataset[prop] }
+  if (fragment) {
+    for (let prop in fragment.dataset) {
+      app.dataset[`fragmentState${upperFirst(prop)}`] = fragment.dataset[prop]
+    }
+  }
 }
